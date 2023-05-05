40ff1878064b4d7425669985eff6d298c4dccdef
raed <raed.alkhanbashi@gmail.com>
2023-05-05 11:23:22 -0700

    add public class Pastry, and pass test 2

--- PierresBakery.Tests/BreadTests.cs
+++ PierresBakery.Tests/BreadTests.cs
@@ -15,5 +15,12 @@ namespace PierresBakery.Tests
             decimal totalCost = bread.GetOrderCost(2);
             Assert.AreEqual(10m, totalCost);
         }
+        [TestMethod]
+        public void TestBreadCost_ThreeLoaves()
+        {
+          Bread bread = new Bread();
+          decimal totalCost = bread.GetOrderCost(3);
+          Assert.AreEqual(10m, totalCost);
+        }
     }
 }
--- PierresBakery/Modles/Bread.cs
+++ PierresBakery/Modles/Bread.cs
@@ -19,3 +19,23 @@ namespace PierresBakery.Models
         }
     }
 }
+
+public class Pastry
+{
+  int PastryCount { get; set; } = 0;
+  decimal PastryPrice { get; set; } = 2.0m;
+  decimal GetOrderCost(int pastryCount)
+  {
+    PastryCount = pastryCount;
+    decimal totalCost = 0m;
+    if (pastryCount >= 3)
+    {
+      totalCost = (pastryCount / 3) * 5m + (pastryCount %3) * PastryPrice;
+    }
+    else
+    {
+      totalCost = pastryCount * pastryCount * PastryPrice;
+    }
+    return totalCost;
+  }
+}
