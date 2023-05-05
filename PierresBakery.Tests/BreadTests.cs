e14aabf45922639ee6f3ebdb988d076fc88233d4
raed <raed.alkhanbashi@gmail.com>
2023-05-05 10:50:19 -0700

    test pass for BreadCost

---
+++ PierresBakery.Tests/BreadTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+using System;
+
+
+namespace PierresBakery.Tests
+{
+    [TestClass]
+    public class BakeryTests
+    {
+        [TestMethod]
+        public void TestBreadCost_TwoLoaves()
+        {
+            Bread bread = new Bread();
+            decimal totalCost = bread.GetOrderCost(2);
+            Assert.AreEqual(10m, totalCost);
+        }
+    }
+}
---
+++ PierresBakery/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PierresBakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Pierre's Bakery!");
+            Console.WriteLine("Bread: $5 each, Buy 2, get 1 free");
+            Console.WriteLine("Pastry: $2 each, 3 for $5");
+
+            // code to handle user input and calculate order total would go here
+
+            Console.ReadLine();
+        }
+    }
+}
