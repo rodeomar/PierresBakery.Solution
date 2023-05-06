da79f09fee69654dd17209f4d838cc474a13ffad
raed <raed.alkhanbashi@gmail.com>
2023-05-05 19:15:22 -0700

    add userInput and breadCost, pastryCost, and orderTotal

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,21 +1,36 @@
 using System;
+using System.Collections.Generic;
+using PierresBakery.Models;
 
 namespace PierresBakery
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Pierre's Bakery!");
-            Console.WriteLine("Bread: $5 each, Buy 2, get 1 free");
-            Console.WriteLine("Pastry: $2 each, 3 for $5");
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We have two items: Bread and Pastry");
+      Console.WriteLine("Here are the deals we offer:");
+      Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
+      Console.WriteLine("Pastry: Buy 3, get 1 free. A single pastry costs $2.");
 
-            Console.WriteLine("How many loaves of bread would you like? ");
-            int pastryCount = int.Parse(Console.ReadLine());
-            
-           Bread bread = new Bread();
-            Pastry pastry = new Pastry(); 
-            // Console.ReadLine();
-        }
+      Console.WriteLine("How many loaves of bread would you like?");
+      int breadCount = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("How many pastries would you like?");
+      int pastryCount = Convert.ToInt32(Console.ReadLine());
+
+
+      Bread bread = new Bread();
+      Pastry pastry = new Pastry();
+
+      int freeBreadCount = breadCount / 3;
+      decimal breadCost = (breadCount - freeBreadCount) * 5m;
+
+      int freePastryCount = pastryCount / 4;
+      decimal pastryCost = (pastryCount - freePastryCount) * 2m;
+
+      Console.WriteLine("Your order total is $" + (breadCost + pastryCost));
     }
+  }
 }
