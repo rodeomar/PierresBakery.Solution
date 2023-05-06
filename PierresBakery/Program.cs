f56d9bd2c04038cce3cb9c5e299dd42f0c8726fa
raed <raed.alkhanbashi@gmail.com>
2023-05-05 18:59:48 -0700

    add Console read and write to Program.cs

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -10,9 +10,12 @@ namespace PierresBakery
             Console.WriteLine("Bread: $5 each, Buy 2, get 1 free");
             Console.WriteLine("Pastry: $2 each, 3 for $5");
 
-            // code to handle user input and calculate order total would go here
-
-            Console.ReadLine();
+            Console.WriteLine("How many loaves of bread would you like? ");
+            int pastryCount = int.Parse(Console.ReadLine());
+            
+           Bread bread = new Bread();
+            Pastry pastry = new Pastry(); 
+            // Console.ReadLine();
         }
     }
 }
