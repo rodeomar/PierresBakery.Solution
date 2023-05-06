15e032f78ced3f9631936f2e49c103a20737c444
raed <raed.alkhanbashi@gmail.com>
2023-05-06 12:14:17 -0700

    edit READMME

--- PierresBakery.Tests/BreadTests.cs
+++ PierresBakery.Tests/BreadTests.cs
@@ -5,108 +5,108 @@ using System;
 
 namespace PierresBakery.Tests
 {
-    [TestClass]
-    public class BakeryTests
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+    public void TestBreadCost_OneLoaves()
     {
-        [TestMethod]
-        public void TestBreadCost_OneLoaves()
-        {
-            Bread bread = new Bread();
-            decimal totalCost = bread.GetOrderCost(1);
-            Assert.AreEqual(5, totalCost);
-        }
-        [TestMethod]
-        public void TestBreadCost_TwoLoaves()
-        {
-            Bread bread = new Bread();
-            decimal totalCost = bread.GetOrderCost(2);
-            Assert.AreEqual(10, totalCost);
-        }
-        [TestMethod]
-        public void TestBreadCost_ThreeLoaves()
-        {
-          Bread bread = new Bread();
-          decimal totalCost = bread.GetOrderCost(3);
-          Assert.AreEqual(10, totalCost);
-        }
-        [TestMethod]
-           public void TestBreadCost_FourLoaves()
-        {
-          Bread bread = new Bread();
-          decimal totalCost = bread.GetOrderCost(4);
-          Assert.AreEqual(15m, totalCost);
-        }
-           [TestMethod]
-        public void TestBreadCost_FiveLoaves()
-        {
-            Bread bread = new Bread();
-            decimal totalCost = bread.GetOrderCost(5);
-            Assert.AreEqual(20m, totalCost);
-        }
-        [TestMethod]
-        public void TestBreadCost_SixLoaves()
-        {
-            Bread bread = new Bread();
-            decimal totalCost = bread.GetOrderCost(6);
-            Assert.AreEqual(20, totalCost);
-        }
-        [TestMethod]
-        public void TestBreadCost_SevenLoaves()
-        {
-            Bread bread = new Bread();
-            decimal totalCost = bread.GetOrderCost(7);
-            Assert.AreEqual(25, totalCost);
-        }
-        [TestMethod]
-        public void TestPastryCost_OnePastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(1);
-            Assert.AreEqual(2, totalCost);
-        }
-        [TestMethod]
-        public void TestPastryCost_TwoPastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(2);
-            Assert.AreEqual(4, totalCost);
-        }
-        [TestMethod]
-        public void TestPastryCost_ThreePastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(3);
-            Assert.AreEqual(6, totalCost);
-        }
-        public void TestPastryCost_FourPastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(4);
-            Assert.AreEqual(6, totalCost);
-        }
-        public void TestPastryCost_FivePastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(5);
-            Assert.AreEqual(8, totalCost);
-        }
-        public void TestPastryCost_SixPastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(6);
-            Assert.AreEqual(10, totalCost);
-        }
-        public void TestPastryCost_SevenPastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(7);
-            Assert.AreEqual(12, totalCost);
-        }
-        public void TestPastryCost_EightPastry()
-        {
-            Pastry pastry = new Pastry();
-            decimal totalCost = pastry.GetOrderCost(8);
-            Assert.AreEqual(12, totalCost);
-        }
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(1);
+      Assert.AreEqual(5, totalCost);
     }
+    [TestMethod]
+    public void TestBreadCost_TwoLoaves()
+    {
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(2);
+      Assert.AreEqual(10, totalCost);
+    }
+    [TestMethod]
+    public void TestBreadCost_ThreeLoaves()
+    {
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(3);
+      Assert.AreEqual(10, totalCost);
+    }
+    [TestMethod]
+    public void TestBreadCost_FourLoaves()
+    {
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(4);
+      Assert.AreEqual(15m, totalCost);
+    }
+    [TestMethod]
+    public void TestBreadCost_FiveLoaves()
+    {
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(5);
+      Assert.AreEqual(20m, totalCost);
+    }
+    [TestMethod]
+    public void TestBreadCost_SixLoaves()
+    {
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(6);
+      Assert.AreEqual(20, totalCost);
+    }
+    [TestMethod]
+    public void TestBreadCost_SevenLoaves()
+    {
+      Bread bread = new Bread();
+      decimal totalCost = bread.GetOrderCost(7);
+      Assert.AreEqual(25, totalCost);
+    }
+    [TestMethod]
+    public void TestPastryCost_OnePastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(1);
+      Assert.AreEqual(2, totalCost);
+    }
+    [TestMethod]
+    public void TestPastryCost_TwoPastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(2);
+      Assert.AreEqual(4, totalCost);
+    }
+    [TestMethod]
+    public void TestPastryCost_ThreePastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(3);
+      Assert.AreEqual(6, totalCost);
+    }
+    public void TestPastryCost_FourPastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(4);
+      Assert.AreEqual(6, totalCost);
+    }
+    public void TestPastryCost_FivePastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(5);
+      Assert.AreEqual(8, totalCost);
+    }
+    public void TestPastryCost_SixPastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(6);
+      Assert.AreEqual(10, totalCost);
+    }
+    public void TestPastryCost_SevenPastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(7);
+      Assert.AreEqual(12, totalCost);
+    }
+    public void TestPastryCost_EightPastry()
+    {
+      Pastry pastry = new Pastry();
+      decimal totalCost = pastry.GetOrderCost(8);
+      Assert.AreEqual(12, totalCost);
+    }
+  }
 }
---
+++ README.md
@@ -0,0 +1,31 @@
+Pierres Bakery
+
+By Raed Alkhanbasahi
+
+Technologies Used
+* C#
+* .NET
+
+Description/
+This application for a bakery that allows users to order bread and pastry with special deals. The user can select the quantity of bread and pastry they want, and the application will calculate the total cost based on the deals. 
+
+Getting Started/
+To get started with this application, follow these steps:
+1. Clone the repository to your local machine.
+2. Open the application in Visual Studio.
+3. Build the application to make sure there are no errors.
+4. Run the project to start the application.
+
+Usage/
+To use the application, follow these steps:
+1. Enter the quantity of bread and pastry you want to order.
+2. Press Enter to see the total cost.
+3. Type "Y" if you want to order more or "N" if you don't.
+4. If you typed "Y", enter the quantity of bread and pastry you want to order again. Otherwise, the application will exit.
+
+Known Bug/
+None.
+
+License/
+Please let me know if you have any questions or concerns at raed@alkhanbashi.gmail.com
+Copyright (c) 2023 Raed Alkhanbashi
